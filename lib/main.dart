2caa85708c20561b1011acaae79f475af1f0633c
Riley Clauss <riley.clauss@gmail.com>
2018-10-02 11:05:44 -0400

    Implemented but not tested
    I added almost a complete academics page but did not test it. I wil test it next class (my AVD is missing, I don't know why?)
    I also attempted to add a splashscreen that doesn't seem to work well at all. I also added a stack for the background.

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,57 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_custom_tabs/flutter_custom_tabs.dart';
 import 'dart:ui';
+import 'package:splashscreen/splashscreen.dart';
 import 'dart:async';
 
-void main() {
-  //launch point for the application. Best practice is for main to be
-  runApp(
-      new FlutterView()); //empty except for the runApp which begins the build process.
-} //goto line 12
+void main(){
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+}
 
-class FlutterView extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return new SplashScreen(
+        seconds: 14,
+        navigateAfterSeconds: new AfterSplash(),
+        title: new Text('Welcome In SplashScreen',
+          style: new TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20.0
+          ),),
+        image: new Image.asset('image/splashscreen.png'),
+        backgroundColor: Colors.black,
+        styleTextUnderTheLoader: new TextStyle(),
+        photoSize: 100.0,
+        onClick: ()=>print("Click!"),
+        loaderColor: Colors.yellow
+    );
+  }
+}
+
+class AfterSplash extends StatelessWidget {
   //main widget for the overall scaffold
   @override //and all buttons
   Widget build(BuildContext context) {
-    //MyHomePage is a seperate class because of other functions
+    //MyHomePage is a separate class because of other functions
     return new MaterialApp(
       //MaterialApp is a flutter object
       theme: ThemeData(
         brightness: Brightness.dark,
         primaryColor: Colors.black,
         accentColor: Colors.grey,
-        buttonColor: Colors.white54,
+        buttonColor: Colors.white24,
+        textTheme: Theme.of(context).textTheme.apply(
+          bodyColor: Colors.yellow,
+          displayColor: Colors.yellow,
+        )
       ),
       home: new MyHomePage(), //goto line 21
     );
@@ -46,38 +77,45 @@ class MyHomePage extends StatelessWidget {
       appBar: new AppBar(
         //AppBar is the title at the top
         title: new Text('PHS Mobile'), //may remove if we get a nice background
-        //background of titlebar. these colors don't seem to be implementing, however @TODO
+        //background of titlebar.
       ),
-      body: new Container(
-        //Container for icons and text
-        child: new GridView.count(
-            //GridView.count is most effecient for formatting and adjusting to various screen sizes
-            crossAxisCount: 4, //four icons per row
-            mainAxisSpacing: 10.0, // 10px between tops and bottoms
-            crossAxisSpacing: 8.0, // 8px side to side
-            childAspectRatio: (itemWidth /
-                itemHeight), //aspect ratio forced for items, in this case, iconTxtBtn
-            padding: const EdgeInsets.only(
-                //padding around the GridView itself
-                left: 6.0,
-                top: 0.0,
-                right: 6.0,
-                bottom: 0.0),
-            children: <Widget>[
-              new iconTxtBtn('https://pnn.phmschools.org/', 'images/newsicon.png', 'News'),
-              new iconTxtBtn('https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffcc33&src=phm.k12.in.us_jdattjdnmubmjev1moof0l8bh8%40group.calendar.google.com&color=%231B887A&ctz=America%2FNew_York/',
-                  'images/calendaricon.png', 'Calendar'), //              iconTxtButton.dart file
-              new iconTxtBtn('https://penn.phmschools.org/students-and-parents','images/sapicon.png', 'Students and Parents'), //and a title to be displayed under it
-              new iconTxtBtn('academics', 'images/academicsicon.png', 'Academics',),
-              new iconTxtBtn('https://penn.phmschools.org/counseling', 'images/counseling.png', 'Counseling'),
-              new iconTxtBtn('https://pennant.phmschools.org/', 'images/athleticsicon.png', 'Athletics'),
-              new iconTxtBtn('https://penn.phmschools.org/fine-arts-communications-academy-0', 'images/fineartsicon.png', 'Fine Arts'),
-              new iconTxtBtn('https://penn.phmschools.org/students-and-parents/get-involved/students/clubs', 'images/clubsicon.png', 'Clubs'),
-              new iconTxtBtn('https://www.phmschools.org/user', 'images/keyicon.png', 'Staff Links'),
-              new iconTxtBtn('https://penn.phmschools.org/', 'images/pennwebsite.png', 'Penn Website'),
-              new iconTxtBtn('https://penn.phmschools.org/staff-directory', 'images/contactusicon.png', 'Contact Us'),
-              new iconTxtBtn('https://penn.phmschools.org/about', 'images/abouticon.png', 'About Penn'),
-            ]),
+      body: new Stack(
+              children: <Widget>[
+                new Container(decoration: new BoxDecoration(
+                  image: new DecorationImage(image: new AssetImage("images/background.png"), fit: BoxFit.cover,),
+                ),),
+                new Container(
+                //Container for icons and text
+                child: new GridView.count(
+                  //GridView.count is most efficient for formatting and adjusting to various screen sizes
+                    crossAxisCount: 4, //four icons per row
+                    mainAxisSpacing: 10.0, // 10px between tops and bottoms
+                    crossAxisSpacing: 8.0, // 8px side to side
+                    childAspectRatio: (itemWidth /
+                        itemHeight), //aspect ratio forced for items, in this case, iconTxtBtn
+                    padding: const EdgeInsets.only(
+                      //padding around the GridView itself
+                        left: 6.0,
+                        top: 0.0,
+                        right: 6.0,
+                        bottom: 0.0),
+                    children: <Widget>[
+                      new iconTxtBtn('https://pnn.phmschools.org/', 'images/newsicon.png', 'News'),
+                      new iconTxtBtn('https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffcc33&src=phm.k12.in.us_jdattjdnmubmjev1moof0l8bh8%40group.calendar.google.com&color=%231B887A&ctz=America%2FNew_York/',
+                          'images/calendaricon.png', 'Calendar'), //              iconTxtButton.dart file
+                      new iconTxtBtn('https://penn.phmschools.org/students-and-parents','images/sapicon.png', 'Students and Parents'), //and a title to be displayed under it
+                      new iconTxtBtn('academics', 'images/academicsicon.png', 'Academics',),
+                      new iconTxtBtn('https://penn.phmschools.org/counseling', 'images/counseling.png', 'Counseling'),
+                      new iconTxtBtn('https://pennant.phmschools.org/', 'images/athleticsicon.png', 'Athletics'),
+                      new iconTxtBtn('https://penn.phmschools.org/fine-arts-communications-academy-0', 'images/fineartsicon.png', 'Fine Arts'),
+                      new iconTxtBtn('https://penn.phmschools.org/students-and-parents/get-involved/students/clubs', 'images/clubsicon.png', 'Clubs'),
+                      new iconTxtBtn('https://www.phmschools.org/user', 'images/keyicon.png', 'Staff Links'),
+                      new iconTxtBtn('https://penn.phmschools.org/', 'images/pennwebsite.png', 'Penn Website'),
+                      new iconTxtBtn('https://penn.phmschools.org/staff-directory', 'images/contactusicon.png', 'Contact Us'),
+                      new iconTxtBtn('https://penn.phmschools.org/about', 'images/abouticon.png', 'About Penn'),
+                    ]),
+              ),
+              ],
       ),
     );
   }
@@ -91,65 +129,92 @@ class academicsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Academics Links'),
       ),
-      body: new Column(
-
+      body: new ListView(
         children: <Widget>[
           new SizedBox(
                 width: double.infinity,
                 child: new RaisedButton(
                   child: Text('HAC'),
                   onPressed: () {
-                    try {
-                      launch(
-                        'https://hac.phmschools.org/HomeAccess/Account/LogOn?ReturnUrl=%2Fhomeaccess',
-                        option: new CustomTabsOption(
-                          toolbarColor: Colors.black45,
-                          enableDefaultShare: true,
-                          enableUrlBarHiding: true,
-                          showPageTitle: true,
-                          animation: new CustomTabsAnimation.slideIn(),
-                          extraCustomTabs: <String>[
-                            'org.mozilla.firefox',
-                            'com.microsoft.emmx',
-                          ],
-                        ),
-                      );
-                    } catch (e) {
-                      debugPrint(e.toString());
-                    }
+                    _launchURL('https://hac.phmschools.org/HomeAccess/Account/LogOn?ReturnUrl=%2Fhomeaccess', context);
                   },
                 )),
-
+          new Text('Home Access Center is a web-based program that allows students and parents to track check progress and grades, manage schedules, and track attendance. This button will take you to the login page.',
+            style: new TextStyle(
+              color: Colors.white12,
+            ),
+          textAlign: TextAlign.center,),
           new SizedBox(
             width: double.infinity,
             child: new RaisedButton(
               child: Text('Canvas'),
               onPressed: () {
-                try {
-                  launch(
-                    'https://phm.instructure.com/login/ldap',
-                    option: new CustomTabsOption(
-                      toolbarColor: Colors.black45,
-                      enableDefaultShare: true,
-                      enableUrlBarHiding: true,
-                      showPageTitle: true,
-                      animation: new CustomTabsAnimation.slideIn(),
-                      extraCustomTabs: <String>[
-                        'org.mozilla.firefox',
-                        'com.microsoft.emmx',
-                      ],
-                    ),
-                  );
-                } catch (e) {
-                  debugPrint(e.toString());
-                }
+                _launchURL('https://phm.instructure.com/login/ldap', context);
               },
             ),
-          )
+          ),
+          new Text('Canvas is the system Penn teachers and staff use to distribute agendas and assignments to students throughout the year. This button will take you to the login page.',
+          style: new TextStyle(
+            color: Colors.white12,
+          ),
+          textAlign: TextAlign.center,),
+
+          new SizedBox(
+            width: double.infinity,
+            child: new RaisedButton(
+              child: Text('Clever'),
+              onPressed: () {
+                _launchURL('https://clever.com/in/phmschools', context);
+              },
+            ),
+          ),
+          new Text('Clever is a useful hub for all students to access specific links and pages used frequently in their classes. This button will take you to the login page.',
+            style: new TextStyle(
+              color: Colors.white12,
+            ),
+            textAlign: TextAlign.center,),
+
+          new SizedBox(
+            width: double.infinity,
+            child: new RaisedButton(
+              child: Text('Lunch Menus'),
+              onPressed: () {
+                _launchURL('https://www.phmschools.org/food-service/penn-menus', context);
+              },
+            ),
+          ),
+          new Text('The Kingsmen Cafe offers seven different food stations for Penn students to choose from daily. Penn offers both lunch and breakfast to students. This button will take you to the menu.',
+            style: new TextStyle(
+              color: Colors.white12,
+            ),
+            textAlign: TextAlign.center,),
+
+          //
         ],
       ),
     );
   }
+
+  void _launchURL(dest, context) async {
+      try {
+        await launch(
+          dest,
+          option: new CustomTabsOption(
+            toolbarColor: Colors.black45,
+            enableDefaultShare: true,
+            enableUrlBarHiding: true,
+            showPageTitle: true,
+            animation: new CustomTabsAnimation.slideIn(),
+            extraCustomTabs: <String>[
+              'org.mozilla.firefox',
+              'com.microsoft.emmx',
+            ],
+          ),
+        );
+      } catch (e) {
+        debugPrint(e.toString());
+      }
+    }
 }
 
 class iconTxtBtn extends StatelessWidget {
@@ -209,18 +274,13 @@ class iconTxtBtn extends StatelessWidget {
             enableUrlBarHiding: true,
             showPageTitle: true,
             animation: new CustomTabsAnimation.slideIn(),
-            // or user defined animation.
-
             extraCustomTabs: <String>[
-              // ref. https://play.google.com/store/apps/details?id=org.mozilla.firefox
               'org.mozilla.firefox',
-              // ref. https://play.google.com/store/apps/details?id=com.microsoft.emmx
               'com.microsoft.emmx',
             ],
           ),
         );
       } catch (e) {
-        // An exception is thrown if browser app is not installed on Android device.
         debugPrint(e.toString());
       }
     }
