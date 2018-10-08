b23084efb8594b69d28df3288281bd285fa3a454
Riley Clauss <riley.clauss@gmail.com>
2018-10-08 11:06:57 -0400

    Nearing Final Version
    Finished academics and staff links
    Pretty much done now, just need to add notifications and maybe conference with Micah and Lester
    Need to do: Notifications (which'll be hell.)
    Check memory leaks, ensure we follow material design, maybe fix some aesthetics?

--- lib/main.dart
+++ lib/main.dart
@@ -2,60 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_custom_tabs/flutter_custom_tabs.dart';
 import 'dart:ui';
-import 'package:splashscreen/splashscreen.dart';
-import 'dart:async';
 import 'package:flutter/services.dart';
 
-void main(){
-  runApp(new MaterialApp(
-    home: new MyApp(),
-  ));
-  SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.top]);
-}
+void main() {
+  //launch point for the application. Best practice is for main to be
+  runApp(
+      new FlutterView()); //empty except for the runApp which begins the build process.
+} //goto line 12
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => new _MyAppState();
-
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return new SplashScreen(
-        seconds: 14,
-        navigateAfterSeconds: new AfterSplash(),
-        title: new Text('Welcome In SplashScreen',
-          style: new TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 20.0
-          ),),
-        image: new Image.asset('image/splashscreen.png'),
-        backgroundColor: Colors.black,
-        styleTextUnderTheLoader: new TextStyle(),
-        photoSize: 100.0,
-        onClick: ()=>print("Click!"),
-        loaderColor: Colors.yellow
-    );
-  }
-}
-
-class AfterSplash extends StatelessWidget {
+class FlutterView extends StatelessWidget {
   //main widget for the overall scaffold
   @override //and all buttons
   Widget build(BuildContext context) {
-    //MyHomePage is a separate class because of other functions
+    //MyHomePage is a seperate class because of other functions
     return new MaterialApp(
       //MaterialApp is a flutter object
       theme: ThemeData(
         brightness: Brightness.dark,
         primaryColor: Colors.black,
-        accentColor: Colors.grey,
-        buttonColor: Colors.white24,
-        textTheme: Theme.of(context).textTheme.apply(
-          bodyColor: Colors.yellow,
-          displayColor: Colors.yellow,
-        )
+        accentColor: Colors.yellow,
+        buttonColor: Colors.black,
+
       ),
       home: new MyHomePage(), //goto line 21
     );
@@ -68,7 +35,6 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     //deployment for new MyHomePage();
     var size = MediaQuery.of(context).size; //gets the size of the entire screen
     final double itemHeight =
@@ -76,121 +42,160 @@ class MyHomePage extends StatelessWidget {
     final double itemWidth = size.width / 2; //gets screen width
     return new Scaffold(
       //scaffold is the base almost everything is built on. gives name bar and body
-       //background color of icons
+      //background color of icons
       appBar: new AppBar(
         //AppBar is the title at the top
         title: new Text('PHS Mobile'), //may remove if we get a nice background
         //background of titlebar.
       ),
       body: new Stack(
-              children: <Widget>[
-                new Container(decoration: new BoxDecoration(
-                  image: new DecorationImage(image: new AssetImage("images/background.png"), fit: BoxFit.cover,),
-                ),),
-                new Container(
-                //Container for icons and text
-                child: new GridView.count(
-                  //GridView.count is most efficient for formatting and adjusting to various screen sizes
-                    crossAxisCount: 4, //four icons per row
-                    mainAxisSpacing: 10.0, // 10px between tops and bottoms
-                    crossAxisSpacing: 8.0, // 8px side to side
-                    childAspectRatio: (itemWidth /
-                        itemHeight), //aspect ratio forced for items, in this case, iconTxtBtn
-                    padding: const EdgeInsets.only(
-                      //padding around the GridView itself
-                        left: 6.0,
-                        top: 0.0,
-                        right: 6.0,
-                        bottom: 0.0),
-                    children: <Widget>[
-                      new iconTxtBtn('https://pnn.phmschools.org/', 'images/newsicon.png', 'News'),
-                      new iconTxtBtn('https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffcc33&src=phm.k12.in.us_jdattjdnmubmjev1moof0l8bh8%40group.calendar.google.com&color=%231B887A&ctz=America%2FNew_York/',
-                          'images/calendaricon.png', 'Calendar'), //              iconTxtButton.dart file
-                      new iconTxtBtn('https://penn.phmschools.org/students-and-parents','images/sapicon.png', 'Students and Parents'), //and a title to be displayed under it
-                      new iconTxtBtn('academics', 'images/academicsicon.png', 'Academics',),
-                      new iconTxtBtn('https://penn.phmschools.org/counseling', 'images/counseling.png', 'Counseling'),
-                      new iconTxtBtn('https://pennant.phmschools.org/', 'images/athleticsicon.png', 'Athletics'),
-                      new iconTxtBtn('https://penn.phmschools.org/fine-arts-communications-academy-0', 'images/fineartsicon.png', 'Fine Arts'),
-                      new iconTxtBtn('https://penn.phmschools.org/students-and-parents/get-involved/students/clubs', 'images/clubsicon.png', 'Clubs'),
-                      new iconTxtBtn('https://www.phmschools.org/user', 'images/keyicon.png', 'Staff Links'),
-                      new iconTxtBtn('https://penn.phmschools.org/', 'images/pennwebsite.png', 'Penn Website'),
-                      new iconTxtBtn('https://penn.phmschools.org/staff-directory', 'images/contactusicon.png', 'Contact Us'),
-                      new iconTxtBtn('https://penn.phmschools.org/about', 'images/abouticon.png', 'About Penn'),
-                    ]),
+        children: <Widget>[
+          new Container(
+
+            decoration: new BoxDecoration(
+              image: new DecorationImage(
+                image: new AssetImage("images/background.png"),
+                fit: BoxFit.fill,
               ),
-              ],
+            ),
+          ),
+          new Container(
+            //Container for icons and text
+            child: new GridView.count(
+                //GridView.count is most efficient for formatting and adjusting to various screen sizes
+                crossAxisCount: 4, //four icons per row
+                mainAxisSpacing: 10.0, // 10px between tops and bottoms
+                crossAxisSpacing: 8.0, // 8px side to side
+                childAspectRatio: (itemWidth /
+                    itemHeight), //aspect ratio forced for items, in this case, IconTxtBtn
+                padding: const EdgeInsets.only(
+                    //padding around the GridView itself
+                    left: 10.0,
+                    top: 0.0,
+                    right: 10.0,
+                    bottom: 0.0),
+                children: <Widget>[
+                  new IconTxtBtn('https://pnn.phmschools.org/',
+                      'images/newsicon.png', 'News'),
+                  new IconTxtBtn(
+                      'https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffcc33&src=phm.k12.in.us_jdattjdnmubmjev1moof0l8bh8%40group.calendar.google.com&color=%231B887A&ctz=America%2FNew_York/',
+                      'images/calendaricon.png',
+                      'Calendar'), //              IconTxtButton.dart file
+                  new IconTxtBtn(
+                      'https://penn.phmschools.org/students-and-parents',
+                      'images/sapicon.png',
+                      'Students and Parents'), //and a title to be displayed under it
+                  new IconTxtBtn(
+                    'academics',
+                    'images/academicsicon.png',
+                    'Academics',
+                  ),
+                  new IconTxtBtn('https://penn.phmschools.org/counseling',
+                      'images/counseling.png', 'Counseling'),
+                  new IconTxtBtn('https://pennant.phmschools.org/',
+                      'images/athleticsicon.png', 'Athletics'),
+                  new IconTxtBtn(
+                      'https://penn.phmschools.org/fine-arts-communications-academy-0',
+                      'images/fineartsicon.png',
+                      'Fine Arts'),
+                  new IconTxtBtn(
+                      'https://penn.phmschools.org/students-and-parents/get-involved/students/clubs',
+                      'images/clubsicon.png',
+                      'Clubs'),
+                  new IconTxtBtn('stafflinks',
+                      'images/keyicon.png', 'Staff Links'),
+                  new IconTxtBtn('https://penn.phmschools.org/',
+                      'images/pennwebsite.png', 'Penn Website'),
+                  new IconTxtBtn('https://penn.phmschools.org/staff-directory',
+                      'images/contactusicon.png', 'Contact Us'),
+                  new IconTxtBtn('https://penn.phmschools.org/about',
+                      'images/abouticon.png', 'About Penn'),
+                ]),
+          ),
+        ],
       ),
     );
   }
 } //build
 
-class academicsPage extends StatelessWidget {
+class AcademicLinks extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       appBar: AppBar(
         title: Text('Academics Links'),
       ),
       body: new ListView(
         children: <Widget>[
           new SizedBox(
-                width: double.infinity,
-                child: new RaisedButton(
-                  child: Text('HAC'),
-                  onPressed: () {
-                    _launchURL('https://hac.phmschools.org/HomeAccess/Account/LogOn?ReturnUrl=%2Fhomeaccess', context);
-                  },
-                )),
-          new Text('Home Access Center is a web-based program that allows students and parents to track check progress and grades, manage schedules, and track attendance. This button will take you to the login page.',
+              width: double.infinity,
+              child: new RaisedButton(
+                child: Text('HAC', style: new TextStyle(color: Colors.yellow),),
+                onPressed: () {
+                  _launchURL(
+                      'https://hac.phmschools.org/HomeAccess/Account/LogOn?ReturnUrl=%2Fhomeaccess',
+                      context);
+                },
+              )),
+          new Text(
+            'Home Access Center is a web-based program that allows students and parents to track check progress and grades, manage schedules, and track attendance. This will take you to the login page.',
             style: new TextStyle(
-              color: Colors.white12,
+              color: Colors.white,
             ),
-          textAlign: TextAlign.center,),
+            textAlign: TextAlign.center,
+          ),
           new SizedBox(
             width: double.infinity,
             child: new RaisedButton(
-              child: Text('Canvas'),
+              child: Text('Canvas', style: new TextStyle(color: Colors.yellow),),
+
               onPressed: () {
                 _launchURL('https://phm.instructure.com/login/ldap', context);
               },
             ),
           ),
-          new Text('Canvas is the system Penn teachers and staff use to distribute agendas and assignments to students throughout the year. This button will take you to the login page.',
-          style: new TextStyle(
-            color: Colors.white12,
+          new Text(
+            'Canvas is the system Penn teachers and staff use to distribute agendas and assignments to students throughout the year. This will take you to the login page.',
+            style: new TextStyle(
+              color: Colors.white,
+            ),
+            textAlign: TextAlign.center,
           ),
-          textAlign: TextAlign.center,),
 
           new SizedBox(
             width: double.infinity,
             child: new RaisedButton(
-              child: Text('Clever'),
+              child: Text('Clever', style: new TextStyle(color: Colors.yellow),),
               onPressed: () {
                 _launchURL('https://clever.com/in/phmschools', context);
               },
             ),
           ),
-          new Text('Clever is a useful hub for all students to access specific links and pages used frequently in their classes. This button will take you to the login page.',
+          new Text(
+            'Clever is a useful hub for all students to access specific links and pages used frequently in their classes. This will take you to the login page.',
             style: new TextStyle(
-              color: Colors.white12,
+              color: Colors.white,
             ),
-            textAlign: TextAlign.center,),
+            textAlign: TextAlign.center,
+          ),
 
           new SizedBox(
             width: double.infinity,
             child: new RaisedButton(
-              child: Text('Lunch Menus'),
+              child: Text('Lunch Menus', style: new TextStyle(color: Colors.yellow),),
               onPressed: () {
-                _launchURL('https://www.phmschools.org/food-service/penn-menus', context);
+                _launchURL('https://www.phmschools.org/food-service/penn-menus',
+                    context);
               },
             ),
           ),
-          new Text('The Kingsmen Cafe offers seven different food stations for Penn students to choose from daily. Penn offers both lunch and breakfast to students. This button will take you to the menu.',
+          new Text(
+            'The Kingsmen Cafe offers seven different food stations for Penn students to choose from daily. Penn offers both lunch and breakfast to students. This will take you to the menu.',
             style: new TextStyle(
-              color: Colors.white12,
+              color: Colors.white,
             ),
-            textAlign: TextAlign.center,),
+            textAlign: TextAlign.center,
+          ),
 
           //
         ],
@@ -199,33 +204,143 @@ class academicsPage extends StatelessWidget {
   }
 
   void _launchURL(dest, context) async {
-      try {
-        await launch(
-          dest,
-          option: new CustomTabsOption(
-            toolbarColor: Colors.black45,
-            enableDefaultShare: true,
-            enableUrlBarHiding: true,
-            showPageTitle: true,
-            animation: new CustomTabsAnimation.slideIn(),
-            extraCustomTabs: <String>[
-              'org.mozilla.firefox',
-              'com.microsoft.emmx',
-            ],
-          ),
-        );
-      } catch (e) {
-        debugPrint(e.toString());
-      }
+    try {
+      await launch(
+        dest,
+        option: new CustomTabsOption(
+          toolbarColor: Colors.black45,
+          enableDefaultShare: true,
+          enableUrlBarHiding: true,
+          showPageTitle: true,
+          animation: new CustomTabsAnimation.slideIn(),
+          extraCustomTabs: <String>[
+            'org.mozilla.firefox',
+            'com.microsoft.emmx',
+          ],
+        ),
+      );
+    } catch (e) {
+      debugPrint(e.toString());
     }
+  }
 }
 
-class iconTxtBtn extends StatelessWidget {
+
+class StaffLinks extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Staff Links'),
+      ),
+      body: new ListView(
+        children: <Widget>[
+          new SizedBox(
+              width: double.infinity,
+              child: new RaisedButton(
+                child: Text('TAC', style: new TextStyle(color: Colors.yellow),),
+                onPressed: () {
+                  _launchURL(
+                      'https://eschoolplus.phmschools.org/TAC/Account/LogOn?ReturnUrl=%2fTAC',
+                      context);
+                },
+              )),
+          new Text(
+            'This will take you to the login page for TAC/eSchool.',
+            style: new TextStyle(
+              color: Colors.white,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          new SizedBox(
+            width: double.infinity,
+            child: new RaisedButton(
+              child: Text('Doculivery', style: new TextStyle(color: Colors.yellow),),
+
+              onPressed: () {
+                _launchURL('https://my.doculivery.com/External/PHMSchools/Login.aspx', context);
+              },
+            ),
+          ),
+          new Text(
+            'This will take you to the Doculivery System login page for PHM\'s payroll and paystub information.',
+            style: new TextStyle(
+              color: Colors.white,
+            ),
+            textAlign: TextAlign.center,
+          ),
+
+          new SizedBox(
+            width: double.infinity,
+            child: new RaisedButton(
+              child: Text('Clever', style: new TextStyle(color: Colors.yellow),),
+              onPressed: () {
+                _launchURL('https://clever.com/in/phmschools', context);
+              },
+            ),
+          ),
+          new Text(
+            'Clever is a useful hub for all students to access specific links and pages used frequently in their classes. This will take you to the login page.',
+            style: new TextStyle(
+              color: Colors.white,
+            ),
+            textAlign: TextAlign.center,
+          ),
+
+          new SizedBox(
+            width: double.infinity,
+            child: new RaisedButton(
+              child: Text('Canvas', style: new TextStyle(color: Colors.yellow),),
+              onPressed: () {
+                _launchURL('https://phm.instructure.com/login/ldap',
+                    context);
+              },
+            ),
+          ),
+          new Text(
+            'Canvas is the system Penn teachers and staff use to distribute agendas and assignments to students throughout the year. This will take you to the login page.',
+            style: new TextStyle(
+              color: Colors.white,
+            ),
+            textAlign: TextAlign.center,
+          ),
+
+          //
+        ],
+      ),
+    );
+  }
+
+  void _launchURL(dest, context) async {
+    try {
+      await launch(
+        dest,
+        option: new CustomTabsOption(
+          toolbarColor: Colors.black45,
+          enableDefaultShare: true,
+          enableUrlBarHiding: true,
+          showPageTitle: true,
+          animation: new CustomTabsAnimation.slideIn(),
+          extraCustomTabs: <String>[
+            'org.mozilla.firefox',
+            'com.microsoft.emmx',
+          ],
+        ),
+      );
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+}
+
+
+
+class IconTxtBtn extends StatelessWidget {
   String _dest;
   String _icon;
   String _txt;
 
-  iconTxtBtn(dest, icon, txt) {
+  IconTxtBtn(dest, icon, txt) {
     this._dest = dest;
     this._icon = icon;
     this._txt = txt;
@@ -264,9 +379,13 @@ class iconTxtBtn extends StatelessWidget {
     if (dest == 'academics') {
       Navigator.push(
         context,
-        MaterialPageRoute(builder: (context) => academicsPage()),
+        MaterialPageRoute(builder: (context) => AcademicLinks()),
       );
     } else if (dest == 'stafflinks') {
+      Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => StaffLinks()),
+      );
     } else {
       try {
         await launch(
